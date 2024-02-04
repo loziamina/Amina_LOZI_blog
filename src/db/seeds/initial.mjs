7337922daf65c311c8a9bd89e6fce1dc20c5e3f8
loziamina <lozi.amina.sio@gmail.com>
2024-02-04 20:29:27 +0100

    resolve seed bug

--- src/db/seeds/initial.mjs
+++ src/db/seeds/initial.mjs
@@ -1,33 +1,37 @@
 import { faker } from "@faker-js/faker"
 
-export const seed = async (db) => {
-  await db("comments").del()
-  await db("posts").del()
-  await db("users").del()
+export const seed = async db => {
+  await db('comments').del()
+  await db('posts').del()
+  await db('users').del()
 
-
-  await db("users").insert(
-    [...Array(5)].map(() => ({
+  const usersData = [...Array(5)].map(() => ({
       email: faker.internet.email(),
       passwordHash: "alskdjalsdkjasdlkj",
       passwordSalt: "alskdjalsdkjasdlkj",
-    }))
-  )
+  }))
+  await db('users').insert(usersData)
 
-  const users = await db("users").select("idUser");
-  await db("posts").insert(
-    [...Array(100)].map(() => ({
-      description: faker.lorem.sentence(),
-      users_Id: faker.random.arrayElement(users).id,
-    }))
-  )
+  const users = await db('users').select('idUser')
 
-  const posts = await db("posts").select("idPosts");
-  await db("comments").insert(
-    [...Array(500)].map(() => ({
-      description: faker.lorem.sentence(),
-      users_Comments_Id: faker.random.arrayElement(users).id,
-      posts_Comments_Id: faker.random.arrayElement(posts).id,
-    }))
-  )
+  for (const user of users) {
+    await db('posts').insert(
+      [...Array(10)].map(() => ({
+        description: faker.lorem.paragraph(),
+        users_Id: user.idUser,
+      }))
+    )
+  }
+
+  const posts = await db('posts').select('idPosts')
+
+  posts.forEach(async post => {
+    await db('comments').insert(
+      [...Array(5)].map(() => ({
+        description:  faker.word.words({ count: { min: 2, max: 10 } }),
+        posts_Comments_Id: faker.number.int({ min: 0, max: categories.length - 1 }).idPosts,
+        users_Comments_Id: faker.number.int({ min: 0, max: categories.length - 1 }).idUser,
+      }))
+    )
+  })
 }
